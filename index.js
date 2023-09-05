b76f1f2b601fd8251956ea87129a736300625409
JP <juanpablocapilla@gmail.com>
2023-09-04 21:27:47 -0300

    database loging user

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const Animal = mongoose.model('Animal', new mongoose.Schema({
 
 const app = express()
 
-mongoose.connect('mongodb://nico:password@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://juampi:password@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... chanchitos...')
+  console.log('listando... ')
   const animales = await Animal.find();
   return res.send(animales)
 })
 app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'Chanchito', estado: 'Feliz' })
-  return res.send('ok')
+  console.log('creando item...')
+  await Animal.create({ tipo: 'Perro', estado: 'durmiendo' })
+  return res.send(`Item creado! `)
 })
 
 app.listen(3000, () => console.log('listening...'))
